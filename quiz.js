7737c23755628c445151be89e19e8a8e3fcbcdd6
Oliver White <white_oliver@comcast.net>
2016-10-19 13:36:31 -0500

    added some css and js event listeners

--- quiz.js
+++ quiz.js
@@ -1,9 +1,43 @@
-document.getElementById("tree_char").addEventListener("",);
-document.getElementById("tree_button").addEventListener("click",);
-document.getElementById("tree_height").addEventListener("click", 'keyup', function (event) {
+var user_tree_height;
+var user_tree_char;
+
+document.getElementById("tree_height").addEventListener('keyup', function (event) {
   if (event.which === 13) {
+  	eval_tree_height();
   }
 });
+
+document.getElementById("tree_char").addEventListener('keyup', function (event) {
+  if (event.which === 13) {
+  	eval_tree_char();
+  }
+});
+
+document.getElementById("tree_button").addEventListener("click", function (event) {
+	eval_tree_height() + eval_tree_char();
+});
+
+function eval_tree_height() {
+
+	user_tree_height = document.getElementById("tree_height").value
+
+	console.log(user_tree_height);
+}
+
+function eval_tree_char() {
+	user_tree_char = document.getElementById("tree_char").value
+
+	console.log(user_tree_char);
+}
+
+
+
+
+
+
+
+
+
 
 
 
