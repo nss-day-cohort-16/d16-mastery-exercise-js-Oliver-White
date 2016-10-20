66bda4bf15828ad84f4954d1c4660bde67709843
Oliver White <white_oliver@comcast.net>
2016-10-20 12:14:23 -0500

    added event listeners

--- quiz.js
+++ quiz.js
@@ -1,54 +1,64 @@
-var user_tree_height;
-var user_tree_char;
+var tree = {
+    height: 0,
+    char: ''
+};
+var tree_height = document.getElementById("tree_height");
+var tree_char = document.getElementById("tree_char");
+var tree_button = document.getElementById("tree_button")
 
-document.getElementById("tree_height").addEventListener('keyup', function (event) {
-  if (event.which === 13) {
-  	eval_tree_height();
+
+tree_height.addEventListener('keyup', function (event) {
+ if (event.which === 13) {
+     set_height(event.target);
+ }
+});
+
+tree_char.addEventListener('keyup', function (event) {
+ if (event.which === 13) {
+     set_char(event.target);
+ }
+});
+
+tree_button.addEventListener("click", function (event) {
+    set_height_and_char();
+});
+
+function set_height_and_char() {
+  if(tree_height.value > 0) {
+    set_height(tree_height.value);
+  } else {
+    alert('You must set a height value.');
   }
-});
 
-document.getElementById("tree_char").addEventListener('keyup', function (event) {
-  if (event.which === 13) {
-  	eval_tree_char();
+  if(tree_char.value === '') {
+    alert('You must enter a character for your tree');
+  } else {
+    set_char(tree_char.value);
   }
-});
-
-document.getElementById("tree_button").addEventListener("click", function (event) {
-	eval_tree_height() + eval_tree_char();
-});
-
-function eval_tree_height() {
-
-	user_tree_height = document.getElementById("tree_height").value
-
-	console.log(user_tree_height);
+  console.log("Updated tree height to " + tree.height + " and char to " + tree.char);
 }
 
-function eval_tree_char() {
-	user_tree_char = document.getElementById("tree_char").value
-
-	console.log(user_tree_char);
+function set_height(height) {
+  tree.height = height;
+  console.log("Updated tree height to " + tree.height);
 }
 
+function set_char(char) {
+  tree.char = char;
+  console.log("Updated tree char to " + tree.char);
+}
 
+function grow_tree (tree) {
+  var spaces = tree.height - 1;
+  var char = tree.char;
+  var char_string = "";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  for (var i = 0, j = spaces; i < tree.height; i++, j--) {
+    char_string += char;
+    console.log(" ".repeat(j) + char_string);
+    char_string += char;
+  }
+}
 
 
 
